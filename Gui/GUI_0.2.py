e28819a160f4a243e62aac12bacf7e0a798d311d
poopPOT101 <stryder.dominique@gmail.com>
2020-05-11 12:19:53 -0700

    Removing a useless file

--- Gui/GUI_0.2.py
+++ Gui/GUI_0.2.py
@@ -1,107 +1,26 @@
-import tkinter as tk
-import PIL
-
+"""
+Best Version of Gui So Far
+"""
 import os
+import PIL
+import tkinter as tk
+from PIL import Image, ImageTk
 
 
-
-
-#All Perams#
-BG_COLOR = '#00E5B4'
-ACTIVE_BG_COLOR = '#bdffed'
-DISPLAY_BAR_COLOR = '#00b584'
-ICON_SIZE = 128, 128
-HEIGHT = 480
-WIDTH = 720
-App_Images = ('', 'Images\Settings.png',
-                  'Images\Settings.png',
-                  'Images\Settings.png',
-                  'Images\Settings.png',
-                  'Images\Settings.png',
-                  'Images\Settings.png')
-
-#root = tk.Toplevel()
-root = tk.Tk()
-
-def Set_Icon(Image=''):
-    PIL_image=PIL.Image.open(Image)
-    PIL_image.thumbnail(ICON_SIZE)
-    app_icon = PIL.ImageTk.PhotoImage(PIL_image)          
-    return app_icon
+Apps=[]
+class App():
     
-#Canvas#
-main_canvas = tk.Canvas(root, height=HEIGHT,
-                              width=WIDTH, 
-                              bg=BG_COLOR)
-main_canvas.pack()
-
-#Frame#
-
-display_bar= tk.Frame(main_canvas, bg=DISPLAY_BAR_COLOR)
-display_bar.place(relwidth=1, relheight=0.1, relx=0., rely=0)
-
-
-
-
-"""
-APPS
-"""
-Apps=['Settings', 'Weather', 'Tuesday', 'Clock', 'Music', 'PC Mode']
-
-#Application One (Settings)#
-open_app_1 = tk.Button(root,
-                       image = Set_Icon(Image=App_Images[1]),
-                       borderwidth=0, 
-                       height=ICON_SIZE[0],
-                       width=ICON_SIZE[1],
-                       activebackground=ACTIVE_BG_COLOR, bg=BG_COLOR)
-open_app_1.place(relx=.1, rely=.2)
-
-"""
-
-#Application Two (Weather)#
-open_app_2 = tk.Button(root,
-                       image=SetIcon(App_Images[2]),
-                       borderwidth=0, 
-                       height=ICON_SIZE[0],
-                       width=ICON_SIZE[1],
-                       activebackground=ACTIVE_BG_COLOR, bg=BG_COLOR)
-open_app_2.place(relx=.4, rely=.2)
-
-#Application Three (Tuesday)#
-open_app_3 = tk.Button(root,
-                       image=SetIcon(App_Images[3]),
-                       borderwidth=0, 
-                       height=ICON_SIZE[0],
-                       width=ICON_SIZE[1],
-                       activebackground=ACTIVE_BG_COLOR, bg=BG_COLOR)
-open_app_3.place(relx=.7, rely=.2)
-
-#Application Four (Clock)#
-open_app_4 = tk.Button(root,
-                       image=SetIcon(App_Images[4]),
-                       borderwidth=0, 
-                       height=ICON_SIZE[0],
-                       width=ICON_SIZE[1],
-                       activebackground=ACTIVE_BG_COLOR, bg=BG_COLOR)
-open_app_4.place(relx=.1, rely=.6)
-
-#Application Five (Music)#
-open_app_5 = tk.Button(root,
-                       image=SetIcon(App_Images[5]),
-                       borderwidth=0, 
-                       height=ICON_SIZE[0],
-                       width=ICON_SIZE[1],
-                       activebackground=ACTIVE_BG_COLOR, bg=BG_COLOR)
-open_app_5.place(relx=.4, rely=.6)
-
-#Application Six (DesktopMode)#
-open_app_6 = tk.Button(root,
-                       image=SetIcon(App_Images[6]),
-                       borderwidth=0, 
-                       height=ICON_SIZE[0],
-                       width=ICON_SIZE[1],
-                       activebackground=ACTIVE_BG_COLOR, bg=BG_COLOR)
-open_app_6.place(relx=.7, rely=.6)
-"""
-root.mainloop()
+    def __init__(self, icon='', description=''):
+        Apps.append(self)
+        index=0
+        for x in Apps:
+            index+=1
+        self.icon=icon
+        self.index=index
+        self.description=description
+        
+Weather=App('Images\Weather_Icon.png', 'An App for Weather... lol')
+Settings=App('Images\Settings.png', 'Play With Tuesdays Settings ;) ')
+Strunes=App('Images\Music_Icon.png', 'To Jam Out When You are Likely a Lonely Loser')
+Clock=App('Images\Clocks.png', 'To Wake Yo Stupid Ass Up')
+Tuesday=App('Images\AI.png', 'For all your robot needs')
